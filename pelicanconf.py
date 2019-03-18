3bec4a5c000b3ea074e1ec93390124939c9e6076
qiuliuquan <qiuliuquan@geekfinancer.com>
2019-03-18 16:30:24 +0800

    new_theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -31,5 +31,6 @@ SOCIAL = (('You can add links in your config file', '#'),
 
 DEFAULT_PAGINATION = 10
 
+THEME = 'pelican-clean-blog'
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
