8f01ea4667635f3132569a96a84ecf92794b17c7
qiuliuquan <qiuliuquan@geekfinancer.com>
2019-03-25 13:30:08 +0800

    更换theme为pelican-clean-blog

--- pelicanconf.py
+++ pelicanconf.py
@@ -34,6 +34,6 @@ DEFAULT_PAGINATION = 10
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
-THEME = "attila"
+THEME = "pelican-clean-blog"
 PLUGINS = ['pelican-plugins.render_math']
 STATIC_PATHS = ['media']
